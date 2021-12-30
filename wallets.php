d3290a6c2ce7330bc7eb52cef60284c37e973a3b
firestorm40 <cristian@firestorm.ro>
2021-12-30 14:51:02 +0200

    - added exchange cold wallet tags

--- wallets.php
+++ wallets.php
@@ -9,8 +9,11 @@
 $known_wallets = [
     "16LUmwUnU9M4Wn92nrvCStj83LDCRwvAaSio6Xtb3yvqqqCCz" => ["Genesis", "info"],
     "13fiCRZHPqcCFvQvuggKEjDvFsVLmwoavaBw1ng5PdSKvCUGp" => ["Genesis", "info"],
+    "1ixianinfinimine234234234234234234234234234242HP"  => ["Infinimine", "secondary"],
     "4uuXowvxs3pWUKtGuBd9sypn2UmnLpygXHnCJ22X8YYdtry7NqFyZxm3otzJyHgVP" => ["IxiBridge", "primary"],
-    "1ixianinfinimine234234234234234234234234234242HP"  => ["Infinimine", "secondary"]
+    "4kUY8jDNxEoRJQweiLgUtbdWDaLgXMC6zHWPkfsY8gU9bmyxTth6J22mGQddrX4qu" => ["ViteX", "primary"],
+    "4WzxTwYMmAPHYK4RQjwaVAvHuopxGy59oNa5vuPwLs6tUkHoZwD5jyJZUgXFxfv4J" => ["ViteX", "primary"],
+    "3XZjEorLSEDWSCaJE2L3qrsmUhFfjeWAqv4vfE9i1B4S5H1p3Wv3kpvqjyuupspjb" => ["ViteX", "primary"]
 ];
 
 ?>
